d70c62adffe866be7ddb83f04bbb6a21d097ef17
Reza <remo0002@stud.ek.dk>
2026-05-08 13:09:25 +0200

    Initial commit

---
+++ src/main/java/com/example/projectcalculator/ProjectCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectcalculator/ProjectCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
