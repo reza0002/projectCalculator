18bcd1a815bb14276eca0b5f4aa3c8ce9a9cb86b
Reza <remo0002@stud.ek.dk>
2026-05-09 18:17:44 +0200

    User class er lavet - Der lavet 2 forskellige constructors, da jeg tænkte man muligvis kunne oprette en bruger uden en mail???

--- src/main/java/com/example/projectcalculator/model/User.java
+++ src/main/java/com/example/projectcalculator/model/User.java
@@ -1,4 +1,43 @@
 package com.example.projectcalculator.model;
 
 public class User {
+    private String name;
+    private String email;
+    private int password;
+
+
+    public User (String name, String email, int password){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User (String name, int password){
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
 }
