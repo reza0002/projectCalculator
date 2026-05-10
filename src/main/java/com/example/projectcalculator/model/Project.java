f35b4503d3624254a9428d8de3a91b57cc11e38a
HectorPetersen <hector.guldager.petersen@gmail.com>
2026-05-10 16:35:15 +0200

    Project attributter

--- src/main/java/com/example/projectcalculator/model/Project.java
+++ src/main/java/com/example/projectcalculator/model/Project.java
@@ -1,4 +1,43 @@
 package com.example.projectcalculator.model;
 
 public class Project {
+    private int id;
+    private String name;
+    private User projectLeader;
+    public Project (int id, String name, User projectLeader){
+        this.id = id;
+        this.name = name;
+        this.projectLeader = projectLeader;
+    }
+    public Project(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Project(String name, User projectLeader) {
+        this.name = name;
+        this.projectLeader = projectLeader;
+    }
+
+    public Project() {
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public User getProjectLeader() {
+        return projectLeader;
+    }
+    public void setProjectLeader(User projectLeader) {
+        this.projectLeader = projectLeader;
+    }
+
 }
