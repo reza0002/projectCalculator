03a03d856cc3cc44c33664ca129d0f040b090642
linuxcompas <kalikeylogger00@gmail.com>
2026-05-11 11:02:42 +0200

    Attributes, constructor, getters + setters for tasks and subprojects

--- src/main/java/com/example/projectcalculator/model/SubProject.java
+++ src/main/java/com/example/projectcalculator/model/SubProject.java
@@ -1,4 +1,38 @@
 package com.example.projectcalculator.model;
 
 public class SubProject {
+    private String name;
+    private double estPricePerHour;
+    private double estTime;
+
+    public SubProject(String name, double estpricePerHour, double estTime) {
+        this.name = name;
+        estPricePerHour = estpricePerHour;
+        this.estTime = estTime;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getestPricePerHour() {
+        return estPricePerHour;
+    }
+
+    public void setPricePerHour(double pricePerHour) {
+        estPricePerHour = pricePerHour;
+    }
+
+    public double getEstTime() {
+        return estTime;
+    }
+
+    public void setEstTime(double estTime) {
+        this.estTime = estTime;
+    }
 }
+
+
--- src/main/java/com/example/projectcalculator/model/Tasks.java
+++ src/main/java/com/example/projectcalculator/model/Tasks.java
@@ -1,4 +1,37 @@
 package com.example.projectcalculator.model;
 
 public class Tasks {
+    private String name;
+    private double PricePerHour;
+    private double time;
+
+    public Tasks(String name, double pricePerHour, double time) {
+        this.name = name;
+        PricePerHour = pricePerHour;
+        this.time = time;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPricePerHour() {
+        return PricePerHour;
+    }
+
+    public void setPricePerHour(double pricePerHour) {
+        PricePerHour = pricePerHour;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+
+    }
 }
