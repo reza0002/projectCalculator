2c298ec18ea8c77bee3036ff34c10326204bebaa
esp <183955049+espmane@users.noreply.github.com>
2026-05-12 12:47:28 +0200

    prototype html og css fragment og sådan

--- src/main/java/com/example/projectcalculator/ProjectCalculatorApplication.java
+++ src/main/java/com/example/projectcalculator/ProjectCalculatorApplication.java
@@ -2,8 +2,11 @@ package com.example.projectcalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {DataSourceAutoConfiguration.class}
+)
 public class ProjectCalculatorApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/projectcalculator/controller/PrototypeController.java
@@ -0,0 +1,38 @@
+package com.example.projectcalculator.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@org.springframework.stereotype.Controller
+@RequestMapping("/prototype")
+public class PrototypeController {
+
+    @GetMapping("/")
+    public String loginPage() {
+        return "loginPage";
+    }
+
+    @GetMapping("/home")
+    public String homepage() {
+        return "homePage";
+    }
+
+    @GetMapping("/create")
+    public String createProjectPage() {
+        return "projectsPage";
+    }
+
+    @GetMapping("/project-name/subproject")
+    public String subProjectPage() {
+        return "subProjectsPage";
+    }
+
+    @GetMapping("/project-name")
+    public String taskPage() {
+        return "tasksPage";
+    }
+
+    @GetMapping("/project-name/subproject/addtask")
+    public String addTaskPage() {
+        return "addTasks";
+    }
+}
