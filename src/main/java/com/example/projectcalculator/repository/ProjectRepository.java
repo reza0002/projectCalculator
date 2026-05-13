725c58326d33f3daabad3ccc5e45ed7076405556
HectorPetersen <hector.guldager.petersen@gmail.com>
2026-05-13 10:34:03 +0200

    Metoder i Repository og lidt i Service

--- src/main/java/com/example/projectcalculator/repository/ProjectRepository.java
+++ src/main/java/com/example/projectcalculator/repository/ProjectRepository.java
@@ -1,5 +1,92 @@
 package com.example.projectcalculator.repository;
 
+import com.example.projectcalculator.model.Project;
+import com.example.projectcalculator.model.SubProject;
+import com.example.projectcalculator.model.Tasks;
+import com.example.projectcalculator.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.sql.DataSource;
+import java.beans.Transient;
+import java.util.List;
+
 @org.springframework.stereotype.Repository
+
+@Repository
 public class ProjectRepository {
+
+    private final JdbcTemplate template;
+
+    @Autowired
+    private DataSource dataSource;
+
+    public ProjectRepository(JdbcTemplate template) {
+        this.template = template;
+    }
+
+    @Transactional
+    public Project saveProject(Project project) {
+
+    }
+
+    @Transactional
+    public SubProject saveSubProject(SubProject subProject){
+
+    }
+
+    @Transactional
+    public Tasks saveTasks(Tasks tasks){
+
+    }
+
+    @Transactional
+    public boolean deleteProject(Project project){
+
+    }
+
+    @Transactional
+    public boolean deleteSubProject(SubProject subProject){
+
+    }
+
+    @Transactional
+    public boolean deleteTasks(Tasks tasks){
+
+    }
+
+    public User login(String username, String password){
+
+    }
+
+    @Transactional
+    public User findUser(String username){
+
+    }
+
+    private List<Project> findProjects(Project project){
+
+    }
+
+    private List<SubProject> findSubProjects(SubProject subProject){
+
+    }
+
+    private List<Tasks> findTasks(Tasks tasks){
+
+    }
+
+    public void updateProject(String username, Project project){
+
+    }
+
+    public void updateSubProject(SubProject subProject){
+
+    }
+
+    public void updateTasks(Tasks tasks){
+
+    }
 }
--- src/main/java/com/example/projectcalculator/service/ProjectService.java
+++ src/main/java/com/example/projectcalculator/service/ProjectService.java
@@ -1,5 +1,28 @@
 package com.example.projectcalculator.service;
 
-@org.springframework.stereotype.Service
+import com.example.projectcalculator.model.User;
+import com.example.projectcalculator.repository.ProjectRepository;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class ProjectService {
+
+    private final ProjectRepository repository;
+
+    public ProjectService(ProjectRepository repository) {
+        this.repository = repository;
+    }
+
+    public User userLogin(String username, String password){
+        return repository.login(username, password);
+    }
+
+
+    public User findUser(String username) {
+        return repository.findUser(username);
+    }
+
+
+
 }
