c68c9626508bb893d579d1fe5363bd1264f5bce2
HectorPetersen <hector.guldager.petersen@gmail.com>
2026-05-13 10:46:34 +0200

    resten af Service metoder

--- src/main/java/com/example/projectcalculator/service/ProjectService.java
+++ src/main/java/com/example/projectcalculator/service/ProjectService.java
@@ -1,5 +1,8 @@
 package com.example.projectcalculator.service;
 
+import com.example.projectcalculator.model.Project;
+import com.example.projectcalculator.model.SubProject;
+import com.example.projectcalculator.model.Tasks;
 import com.example.projectcalculator.model.User;
 import com.example.projectcalculator.repository.ProjectRepository;
 import org.springframework.stereotype.Service;
@@ -23,6 +26,52 @@ public class ProjectService {
         return repository.findUser(username);
     }
 
+    public Project findProject(Project project){
+        return repository.findProject(project);
+    }
 
+    public SubProject findSubProject(SubProject subProject){
+        return repository.findSubProject(subProject);
+    }
+
+    public Tasks findTasks(Tasks tasks){
+        return repository.findTasks(tasks);
+    }
+
+    public Project saveProject(Project project){
+        return repository.saveProject(project);
+    }
+
+    public SubProject saveSubProject(SubProject subProject){
+        return repository.saveSubProject(subProject);
+    }
+
+    public Tasks saveTasks(Tasks tasks){
+        return repository.saveTasks(tasks);
+    }
+
+    public void updateProject(){
+
+    }
+
+    public void updateSubProject(){
+
+    }
+
+    public void updateTasks(){
+
+    }
+
+    public void deleteProject(Project project){
+
+    }
+
+    public void deleteSubProject(SubProject subProject){
+
+    }
+
+    public void deleteTasks(Tasks tasks){
+
+    }
 
 }
