b29d4fd5713641a70b970d3ef56ba7810be585c9
esp <183955049+espmane@users.noreply.github.com>
2026-05-19 16:09:34 +0200

    tilføjet RowMapper for SubProject

--- src/main/java/com/example/projectcalculator/rowmapper/UserRowMapper.java
+++ src/main/java/com/example/projectcalculator/rowmapper/UserRowMapper.java
@@ -8,10 +8,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserRowMapper implements RowMapper<User> {
-
     @Override
     public User mapRow(@NonNull ResultSet rs, int rowNum) throws SQLException {
         User user = new User();
+
         user.setId(rs.getInt("id"));
         user.setName(rs.getString("name"));
         user.setEmail(rs.getString("email"));
