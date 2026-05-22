23f3dbfbcf678ce222b098c3586a1c5487dc586c
Reza <remo0002@stud.ek.dk>
2026-05-22 13:24:42 +0200

    Exceptions

--- src/main/java/com/example/projectcalculator/exception/GlobalException.java
+++ src/main/java/com/example/projectcalculator/exception/GlobalException.java
@@ -1,6 +1,7 @@
 package com.example.projectcalculator.exception;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,6 +21,13 @@ public class GlobalException {
     public String handleGenericError(Model model, RuntimeException ex) {
         model.addAttribute("status", 500);
         model.addAttribute("message", ex.getMessage() != null ? ex.getMessage() : "Unexpected error");
+        return "error";
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public String handleNotFound (Model model){
+        model.addAttribute("status",404);
+        model.addAttribute("message", "Resource not found");
         return "error";
     }
 }
---
+++ src/main/java/com/example/projectcalculator/exception/ProjectNotFound.java
@@ -0,0 +1,10 @@
+package com.example.projectcalculator.exception;
+
+public class ProjectNotFound extends RuntimeException {
+    public ProjectNotFound(String message) {
+        super(message);
+    }
+}
+
+
+
