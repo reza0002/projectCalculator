143e9b86b9a3b9aad3e70c6146abac7de0a17947
Reza <remo0002@stud.ek.dk>
2026-05-23 15:36:59 +0200

    Nu hvor jeg har det gode net, så værsgo - anbefaler CBB

--- src/test/java/com/example/projectcalculator/Service/ProjcetServiceTest.java
+++ src/test/java/com/example/projectcalculator/Service/ProjcetServiceTest.java
@@ -1,13 +1,18 @@
 package com.example.projectcalculator.Service;
 
 import com.example.projectcalculator.exception.InvalidInputException;
+import com.example.projectcalculator.exception.ProjectNotFound;
+import com.example.projectcalculator.model.Project;
+import com.example.projectcalculator.model.Task;
 import com.example.projectcalculator.repository.ProjectRepository;
 import com.example.projectcalculator.service.ProjectService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 public class ProjcetServiceTest {
     private ProjectRepository repository;
@@ -20,7 +25,38 @@ public class ProjcetServiceTest {
     }
 
     @Test
-    void userLogin_notNull(){
+    void userLogin_nullName(){
         assertThrows(InvalidInputException.class, () -> service.userLogin(null, "blabla"));
     }
+
+    @Test
+    void findProject_notFound(){
+        when(repository.findProject(1)).thenReturn(null);
+        assertThrows(ProjectNotFound.class, ()-> service.findProject(1));
+    }
+
+    @Test
+    void findProject_Exist(){
+        Project project = new Project();
+        project.setId(10);
+        when(repository.findProject(10)).thenReturn(project);
+
+        Project test = service.findProject(10);
+
+        assertEquals(10, test.getId());
+    }
+
+    @Test
+    void saveTask_NegativeHours(){
+        Task task = new Task();
+        task.setName("test");
+        task.setHours(-6);
+        task.setPricePerHour(1200);
+
+        assertThrows(InvalidInputException.class, ()-> service.saveTask(task));
+    }
+
+
+
+
 }
--- src/test/java/controller/ProjectCalculatorControllerTest.java
+++ src/test/java/controller/ProjectCalculatorControllerTest.java
@@ -5,6 +5,7 @@ import com.example.projectcalculator.model.Project;
 import com.example.projectcalculator.service.ProjectService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.bean.override.mockito.MockitoBeans;
 import org.springframework.test.web.servlet.MockMvc;
