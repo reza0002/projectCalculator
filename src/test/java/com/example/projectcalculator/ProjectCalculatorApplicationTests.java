5a7a907dc614e34b971708052bcb203aa3cd830f
linuxcompas <kalikeylogger00@gmail.com>
2026-05-24 16:23:22 +0200

    Contextloads

--- src/test/java/com/example/projectcalculator/ProjectCalculatorApplicationTests.java
+++ src/test/java/com/example/projectcalculator/ProjectCalculatorApplicationTests.java
@@ -2,9 +2,12 @@ package com.example.projectcalculator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProjectCalculatorApplicationTests {
+
 
 	@Test
 	void contextLoads() {
